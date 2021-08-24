95c548c1895fa5c2685a31022dbcb7d7cc8cd642
Anisimova Oxana <ksuschka.an@gmail.com>
2021-08-24 12:10:21 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2000;
+        int mile = 20;
+        int totalBonus = ticketPrice / mile;
+        System.out.println("Количество бонусных миль " + totalBonus);
+    }
+}
